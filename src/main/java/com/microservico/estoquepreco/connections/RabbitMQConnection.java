b405d98900b3e901feb4d7293354fbb139621793
joaothe <joaoded@hotmail.com>
2021-09-06 15:21:45 -0300

    create queue

--- src/main/java/com/microservico/estoquepreco/connections/RabbitMQConnection.java
+++ src/main/java/com/microservico/estoquepreco/connections/RabbitMQConnection.java
@@ -1,6 +1,10 @@
 package com.microservico.estoquepreco.connections;
 
 import com.microservico.estoquepreco.constantes.RabbitmqConstantes;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
@@ -9,6 +13,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitMQConnection {
 	private static final String NOME_EXCHANGE = "amq.direct";
+	
+	private AmqpAdmin amqpAdmin;
+	
+	public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+		this.amqpAdmin = amqpAdmin;
+	}
 	
 	private Queue fila (String nomeFila) {
 		return new Queue(nomeFila, true, false, false);
@@ -19,14 +29,27 @@ public class RabbitMQConnection {
 	}
 	
 	private Binding relacionamento(Queue fila, DirectExchange troca) {
-		return new Binding(fila.getName(), Binding.DestinationType.EXCHANGE, troca.getName(), fila.getName(), null);
+		return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
 	}
 	
+	@PostConstruct
 	private void adiciona() {
 		Queue filaEstoque = this.fila(RabbitmqConstantes.FILA_ESTOQUE);
 		Queue filaPreco = this.fila(RabbitmqConstantes.FILA_PRECO);
 		
 		DirectExchange troca = this.trocaDireta();
+		
+		Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+		Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+		
+		//Criando as filas no RabbitMQ
+		this.amqpAdmin.declareQueue(filaEstoque);
+		this.amqpAdmin.declareQueue(filaPreco);
+		
+		this.amqpAdmin.declareExchange(troca);
+		
+		this.amqpAdmin.declareBinding(ligacaoEstoque);
+		this.amqpAdmin.declareBinding(ligacaoPreco);
 	}
 
 }
